fd2d597249d0e5f766d780708ec664e4f4c2f797
ZYAMUR <zeynepbzd04@gmail.com>
2024-03-28 21:59:43 +0300

    last version of q4

--- src/Main4.java
+++ src/Main4.java
@@ -45,37 +45,62 @@ public class Main4 {
 
         public long shortestPath(List<List<Pair>> adj, int s, int t) {
 
+//            6 8
+//            1 2 2
+//            1 3 4
+//            2 3 1
+//            2 4 7
+//            3 5 3
+//            4 6 1
+//            5 4 2
+//            5 6 5
+//            1 6
+            boolean[] visited = new boolean[adj.size()];
             long[] distance = new long[adj.size()];
+            int edgeNumber = 0;
             for (int i = 0; i < adj.size(); i++) {
                 distance[i] = Long.MAX_VALUE;
             }
             distance[s] = 0;
 
-            Comparator<Pair> comparator = new Comparator<Pair>() {
-                public int compare(Pair pair1, Pair pair2) {
+
+            if(s == t)
+                return 0;
+
+            //edge checking
+            for(int i = 0; i < adj.size(); i++)
+            {
+                edgeNumber += adj.get(i).size();
+            }
+            if(edgeNumber == 0)
+                return -1;
+
+
+
+            Comparator<Main4.TaskA.Pair> comparator = new Comparator<Main4.TaskA.Pair>() {
+                public int compare(Main4.TaskA.Pair pair1, Main4.TaskA.Pair pair2) {
                     return Long.compare(pair1.cost, pair2.cost);
                 }
             };
 
-            // PriorityQueue'yu oluştururken Comparator'ı kullan
-            PriorityQueue<Pair> pq = new PriorityQueue<>(comparator);
+            PriorityQueue<Main4.TaskA.Pair> pq = new PriorityQueue<>(comparator);
+            pq.add(new Main4.TaskA.Pair(s, 0));
 
-            pq.add(new Pair(s, 0));
 
-            boolean[] visited = new boolean[adj.size()];
 
             while (!pq.isEmpty()) {
-                Pair u = pq.poll();
+                Main4.TaskA.Pair u = pq.poll();
                 if (visited[u.node]) {
                     continue;
                 }
                 visited[u.node] = true;
 
+
                 // Relaxation
-                for (Pair neighbor : adj.get(u.node)) {
+                for (Main4.TaskA.Pair neighbor : adj.get(u.node)) {
                     if (!visited[neighbor.node] && distance[u.node] + neighbor.cost < distance[neighbor.node]) {
                         distance[neighbor.node] = distance[u.node] + neighbor.cost;
-                        pq.add(new Pair(neighbor.node, distance[neighbor.node]));
+                        pq.add(new Main4.TaskA.Pair(neighbor.node, distance[neighbor.node]));
                     }
                 }
             }
@@ -100,7 +125,7 @@ public class Main4 {
                 int v = in.nextInt();
                 u--;
                 v--;
-                long cost = in.nextLong(); // Assuming the cost of the edge is given
+                int cost = in.nextInt(); // Assuming the cost of the edge is given
                 adj.get(u).add(new Pair(v, cost)); // Assuming the graph is directed
             }
 
